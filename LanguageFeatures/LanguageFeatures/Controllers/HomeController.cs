8bb708096da37f526ae4cba9fc9201e8a6fa6a1e
Luuk <lbohmer@avans.nl>
2016-12-07 13:47:07 +0100

    Add Chapter 4: Language features

---
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using LanguageFeatures.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Navigate to a URL to show an example";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            //create a new Product object
+            Product product = new Product();
+
+            //set the propert value
+            product.Name = "Kayak";
+
+            //get the property
+            string productName = product.Name;
+
+            //generate the view
+            return View("Result", $"Product name: {productName}");
+        }
+
+        public ViewResult CreateProduct()
+        {
+            //create a new Product object
+            Product product = new Product();
+
+            //set the property values
+            product.ProductID = 100;
+            product.Name = "Kayak";
+            product.Description = "A boat for one person";
+            product.Price = 275M;
+            product.Category = "Watersports";
+
+            return View("Result", $"Category: {product.Category}");
+        }
+
+        public ViewResult CreateCollection()
+        {
+            string[] stringArray = { "apple", "orange", "plum" };
+            List<int> intList = new List<int> { 10, 20, 30, 40 };
+            Dictionary<string, int> myDict = new Dictionary<string, int> { { "apple", 10 }, { "orange", 20 }, { "plum", 30 } };
+
+            return View("Result", stringArray[1]);
+        }
+
+        public ViewResult UseExtension()
+        {
+            //create and populate ShoppingCart
+            ShoppingCart cart = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product {Name = "Kayak", Price = 275M },
+                    new Product {Name = "Lifejacket", Price = 48.95M },
+                    new Product {Name = "Soccer ball", Price = 19.50M },
+                    new Product {Name = "Corner flag", Price = 34.95M }
+                }
+            };
+
+            //get the total value of the products in the cart
+            decimal cartTotal = cart.TotalPrices();
+
+            return View("Result", $"Total: {cartTotal.ToString("N2")}");
+        }
+    }
+}
---
+++ LanguageFeatures/LanguageFeatures/Models/MyAsyncMethods.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public class MyAsyncMethods
+    {
+        public static Task<long?> GetPageLength()
+        {
+            HttpClient client = new HttpClient();
+
+            var httpTask = client.GetAsync("http://apress.com");
+
+            //Do other things while waiter for the http request
+
+            return httpTask.ContinueWith((Task<HttpResponseMessage> antecedant) =>
+            {
+                return antecedant.Result.Content.Headers.ContentLength;
+            });
+        }
+
+        public async static Task<long?> GetPageLengthAsync()
+        {
+            HttpClient client = new HttpClient();
+
+            var httpMessage = await client.GetAsync("http://apress.com");
+
+            //Do other things while waiter for the http request
+            
+            return httpMessage.Content.Headers.ContentLength;
+        }
+    }
+}
