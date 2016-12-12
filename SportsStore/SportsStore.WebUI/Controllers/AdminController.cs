472d23b7b982b25fc860513babc7661d91988f71
Luuk <lbohmer@avans.nl>
2016-12-12 12:58:11 +0100

    Add Chapter 10: SportsStore: Administration

---
+++ SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,63 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository productRepository;
+
+        public AdminController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public ViewResult Index()
+        {
+            return View(productRepository.Products);
+        }
+
+        public ViewResult Edit(int productId)
+        {
+            Product product = productRepository.Products.FirstOrDefault(p => p.ProductID == productId);
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                productRepository.SaveProduct(product);
+                TempData["message"] = $"{product.Name} has ben saved";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                //There is something wrong with the data values
+                return View(product);
+            }
+        }
+
+        public ViewResult Create()
+        {
+            return View("Edit", new Product());
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int productID)
+        {
+            Product deletedProduct = productRepository.DeleteProduct(productID);
+            if(deletedProduct != null)
+            {
+                TempData["message"] = $"{deletedProduct.Name} was deleted";
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
