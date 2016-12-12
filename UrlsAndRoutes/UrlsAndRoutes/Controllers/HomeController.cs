171ac5bd2fe220e2e117e51266ef0207423c021d
Luuk <lbohmer@avans.nl>
2016-12-12 17:19:10 +0100

    Add Chapter 13:  URL Routing (Incomplete)

---
+++ UrlsAndRoutes/UrlsAndRoutes/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Controller = "Home";
+            ViewBag.Action = "Index";
+            return View("ActionName");
+        }
+
+        public ActionResult List()
+        {
+            ViewBag.Controller = "Home";
+            ViewBag.Action = "List";
+            return View("ActionName");
+        }
+
+        public ActionResult CustomVariables(string id)
+        {
+            ViewBag.Controller = "Home";
+            ViewBag.Action = "Index";
+            ViewBag.CustomVariable = id ?? "<no value>";
+            return View();
+        }
+    }
+}
